0b65b54c6de9bcd6dfda0d1389a19a254c5e14da
𝐊ʀɪsʜ 𝐌ɪsʜʀᴀ <163736172+Xkrishmishra@users.noreply.github.com>
2026-06-15 17:13:18 +0530

    .

--- api/instagram/info.js
+++ api/instagram/info.js
@@ -3,6 +3,10 @@ const { promisify } = require("util");
 const execAsync = promisify(exec);
 
 module.exports = async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
   const { url } = req.query;
 
   if (!url) {
